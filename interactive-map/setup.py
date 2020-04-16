27341285a5c3747a188d601586dcc5fa2839e0a2
Dennis Reddyhoff <d.reddyhoff@sheffield.ac.uk>
2020-04-16 14:54:56 +0100

    Add Flask package

--- interactive-map/setup.py
+++ interactive-map/setup.py
@@ -3,9 +3,15 @@ from setuptools import find_packages, setup
 setup(
     name='kampala_map',
     version='1.0.0',
-    packages=find_packages(),
+    packages= find_packages
     zip_safe=False,
     install_requires=[
         'flask',
+        'gpflow',
+        'numpy',
+        'pandas',
+        'google.cloud',
+        'bokeh',
+        'scipy'
     ],
 )
