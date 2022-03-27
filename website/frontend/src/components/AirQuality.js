22f63eebbbe858448505546a7956f42bcbd29263
FaithDaka <faithbusigu8@gmail.com>
2022-03-27 15:32:00 +0300

    Air quality cards carousel

--- website/frontend/src/components/AirQuality.js
+++ website/frontend/src/components/AirQuality.js
@@ -64,6 +64,66 @@ const AirQuality = () => {
                         <Sensor width={464} height={376} />
                     </div>
                 </div>
+                <div className='air-quality-content-sm'>
+                    <section className='carousel'>
+                        <ol className='carousel-viewport'>
+                            <li id='card1' className='carousel-item'>
+                                <div className='unselected'>
+                                    <div className="circle" />
+                                    <div className="text-wrapper">
+                                        <h4>Low cost air quality monitoring devices</h4>
+                                        <div>
+                                            <Checked width={10} height={10} />
+                                            <small>Designed for Africa's varying weather conditions</small>
+                                        </div>
+                                        <div>
+                                            <Checked width={10} height={10} />
+                                            <small>Easy installation and maintenance</small>
+                                        </div>
+                                    </div>
+                                </div>
+                                <div className='carousel-snapper'></div>
+                            </li>
+                            <li id='card2' className='carousel-item'>
+                                <div className='unselected'>
+                                    <div className="circle" />
+                                    <div className="text-wrapper">
+                                        <h4>Access to airquality analytics tools</h4>
+                                        <div>
+                                            <UnChecked width={10} height={10} />
+                                            <small>Easy to use analytics dashboard</small>
+                                        </div>
+                                        <div>
+                                            <UnChecked width={10} height={10} />
+                                            <small>Know your air through our mobile application</small>
+                                        </div>
+                                    </div>
+                                </div>
+                                <div className='carousel-snapper'></div>
+                            </li>
+                            <li id='card3' className='carousel-item'>
+                                <div className='unselected'>
+                                    <div className="circle" />
+                                    <div className="text-wrapper">
+                                        <h4>Robust API intergration</h4>
+                                        <div>
+                                            <UnChecked width={10} height={10} />
+                                            <small>Access air quality data through the AirQo API</small>
+                                        </div>
+                                        <div>
+                                            <UnChecked width={10} height={10} />
+                                            <small>Intergrate the AirQo API with your applications</small>
+                                        </div>
+                                    </div>
+                                </div>
+                                <div className='carousel-snapper'></div>
+                            </li>
+                        </ol>
+                    </section>
+                    <div className="image-container">
+                        <Sensor width={464} height={376} />
+                    </div>
+                </div>
             </div>
         </div>
     )
